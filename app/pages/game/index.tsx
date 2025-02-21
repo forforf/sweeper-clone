41527ab0bc808b6dcc61d0827c6838c68429f51f
forforf <dmarti21@gmail.com>
2025-02-21 15:29:36 -0500

    Multiple pages

---
+++ app/pages/game/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import './game.scss'
+
+export function Game () {
+  return (
+    <div>The Game</div>
+  )
+}
--- app/routeTree.gen.ts
+++ app/routeTree.gen.ts
@@ -11,9 +11,16 @@
 // Import Routes
 
 import { Route as rootRoute } from './routes/__root'
+import { Route as CountImport } from './routes/count'
 import { Route as IndexImport } from './routes/index'
 
 // Create/Update Routes
+
+const CountRoute = CountImport.update({
+  id: '/count',
+  path: '/count',
+  getParentRoute: () => rootRoute,
+} as any)
 
 const IndexRoute = IndexImport.update({
   id: '/',
@@ -32,6 +39,13 @@ declare module '@tanstack/react-router' {
       preLoaderRoute: typeof IndexImport
       parentRoute: typeof rootRoute
     }
+    '/count': {
+      id: '/count'
+      path: '/count'
+      fullPath: '/count'
+      preLoaderRoute: typeof CountImport
+      parentRoute: typeof rootRoute
+    }
   }
 }
 
@@ -39,32 +53,37 @@ declare module '@tanstack/react-router' {
 
 export interface FileRoutesByFullPath {
   '/': typeof IndexRoute
+  '/count': typeof CountRoute
 }
 
 export interface FileRoutesByTo {
   '/': typeof IndexRoute
+  '/count': typeof CountRoute
 }
 
 export interface FileRoutesById {
   __root__: typeof rootRoute
   '/': typeof IndexRoute
+  '/count': typeof CountRoute
 }
 
 export interface FileRouteTypes {
   fileRoutesByFullPath: FileRoutesByFullPath
-  fullPaths: '/'
+  fullPaths: '/' | '/count'
   fileRoutesByTo: FileRoutesByTo
-  to: '/'
-  id: '__root__' | '/'
+  to: '/' | '/count'
+  id: '__root__' | '/' | '/count'
   fileRoutesById: FileRoutesById
 }
 
 export interface RootRouteChildren {
   IndexRoute: typeof IndexRoute
+  CountRoute: typeof CountRoute
 }
 
 const rootRouteChildren: RootRouteChildren = {
   IndexRoute: IndexRoute,
+  CountRoute: CountRoute,
 }
 
 export const routeTree = rootRoute
@@ -77,11 +96,15 @@ export const routeTree = rootRoute
     "__root__": {
       "filePath": "__root.tsx",
       "children": [
-        "/"
+        "/",
+        "/count"
       ]
     },
     "/": {
       "filePath": "index.tsx"
+    },
+    "/count": {
+      "filePath": "count.tsx"
     }
   }
 }
