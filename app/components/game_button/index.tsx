dbf5929394b4cc539d9bb8fb84d2eb4f2bd63f2a
forforf <dmarti21@gmail.com>
2025-02-23 13:15:38 -0500

    Make button active ios

--- app/components/game_button/index.tsx
+++ app/components/game_button/index.tsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './game_button.scss'
 
 export function GameButton() {
+  // TODO: Don't use string,use stronger typing
+  const [buttonStatus, setButtonStatus] = useState<string>('')
+
+  const pushed = () => {
+    setButtonStatus('active')
+  }
+  const released = () => setButtonStatus('')
+
   return (
-    <button className="GameButton"></button>
+    <button className={`GameButton ${buttonStatus}`} onTouchStart={pushed} onTouchEnd={released}></button>
   )
 }
