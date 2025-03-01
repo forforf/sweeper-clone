1fca09bc9facdac8bacaa2be1b56295c003cf413
forforf <dmarti21@gmail.com>
2025-03-01 16:23:29 -0500

    Refactor GameCell.tsx

---
+++ app/components/game_cell/GameCell.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import {GameCellValue} from '@services/game'
+import {GameButton} from '@components/game_button'
+import {Mine} from '@components/mine'
+import './game_cell.scss'
+
+interface GameCellProps {
+  id: string
+  children: GameCellValue
+}
+
+export function GameCell({id, children}: GameCellProps) {
+  let gridUi: React.ReactNode = children
+  if (children === '?') {
+    gridUi =  <GameButton />
+  }
+
+  if (children === 'x') {
+    gridUi = <Mine />
+  }
+
+  return (
+    <div id={id} className="GameCell">
+      <div className="game-cell-value">
+        {gridUi}
+      </div>
+    </div>
+  )
+}
