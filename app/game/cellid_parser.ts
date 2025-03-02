3ec56db1a6756a5cf54a90a77e9d5330783cf237
forforf <dmarti21@gmail.com>
2025-03-02 12:35:44 -0500

    Remove debug log

--- app/game/cellid_parser.ts
+++ app/game/cellid_parser.ts
@@ -3,7 +3,6 @@ const CellIdRegex = /^c(\d+)-r(\d+)$/
 
 export function cellIdIdxs(cellId: string): [number, number] {
   const match = CellIdRegex.exec(cellId) ?? ['', '-1', '-1']
-  console.log('Regex', cellId, match)
   const rowIdx = +match[1]
   const colIdx = +match[2]
   return [colIdx, rowIdx]
