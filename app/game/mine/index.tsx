6f048fac335c2f0ec9130f0fc98c1563570a43dc
forforf <dmarti21@gmail.com>
2025-03-02 12:19:51 -0500

    Move component logic out from index.tsx

--- app/game/mine/index.tsx
+++ app/game/mine/index.tsx
@@ -1,12 +1,1 @@
-import React from 'react'
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-import {faBurst} from '@fortawesome/free-solid-svg-icons'
-import './mine.scss'
-
-export function Mine() {
-  return (
-    <div className="Mine">
-      <FontAwesomeIcon icon={faBurst} />
-    </div>
-  )
-}
+export {Mine} from './Mine'
