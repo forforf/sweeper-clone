f5e989c1075ffe9c7519d9dab40ce1b8f0a15f1d
forforf <dmarti21@gmail.com>
2025-03-02 12:41:30 -0500

    Remove debug logging

--- app/game/Game.tsx
+++ app/game/Game.tsx
@@ -48,28 +48,18 @@ export function Game ({serverState}: GameProps) {
     setGameCells(newGameCells)
   }
 
-  const getGameCellValue = (cellIdxs: GameCellIdx): GameCellValue => {
-    return gameCells[cellIdxs[0]][cellIdxs[1]]
-  }
-
-  console.log('Server state', serverState)
-
   // We will assume there is only a single mouse/pointer (that is multiple buttons can't be long pressed)
   const [pointerState, setPointerState] = useState<PointerStateType>(PointerState.released)
   const pointerPressTimer = useRef<ReturnType<typeof setTimeout> | null>(null)
   const longPressTimeout = 500; //milliseconds
 
   const handlePointerDown = (cellId: string): void=> {
-
-    console.log(pointerState, 'Game -> button pressed')
     setPointerState(PointerState.pressed)
     pointerPressTimer.current = setTimeout(() => {
       setPointerState(PointerState.longPressed)
       const gameCellIdxs = cellIdToCoord(cellId)
       const solutionGameCellValue = getSolutionCellValue(gameCellIdxs)
       setGameCell(solutionGameCellValue, gameCellIdxs)
-      console.log(pointerState, 'Game -> button longPressed')
-
     }, longPressTimeout)
   }
 
@@ -86,8 +76,6 @@ export function Game ({serverState}: GameProps) {
 
   type PointerStateType = typeof PointerState[keyof typeof PointerState]
 
-  console.log('render game')
-  console.log('Pointer State:', pointerState)
   return (
     <GameFunctionsContext.Provider value={gameFunctions}>
       <div className="Game">
