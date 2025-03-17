951c09de19eced186ab5d8b2b10d567ab1d9c840
forforf <dmarti21@gmail.com>
2025-03-17 15:01:40 -0400

    Support tsconfigPaths in vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,5 +1,6 @@
 // Putting this config in app.config.ts inside vite section did not work
-import { defineConfig } from 'vitest/config';
+import { defineConfig } from 'vitest/config'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
   test: {
@@ -7,4 +8,5 @@ export default defineConfig({
     globals: true,
     setupFiles: './vitest.setup.ts'
   },
+  plugins: [tsconfigPaths()]
 });
